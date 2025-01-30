4a6411ad2bb741d54f6206b9f2ed5cdf5c4cdffa
Amrit Gautam <agautam@lxplus931.cern.ch>
2025-01-31 00:07:17 +0100

    updated and agged v2 of main_digitizer that takes vector and computs toa and tot

--- include/focal_sim_digitizer.h
+++ include/focal_sim_digitizer.h
@@ -8,14 +8,27 @@ class focal_sim_digitizer {
 	private:
 		float EnergytoADC = 1.0; //factor that converts energy to ADC value 
         int Pedestal = 0; //pedestal value
-        int toa_thresh = 0; // toa threshold give a time of arrival(TOA) if ADC rise  above this
-        int tot_thresh = 0; // tot threshold give time over threshold (TOT) if ADC falls below this
+        int toa_thresh = 0; //  if ADC rise  above this-->Corresponding time is TOA
+        int tot_thresh = 0; // ADC above tot threshold --> TOT = Time-TOA above tot_thresh 
         bool hasTOA = 0; // this is to make sure that we get tot only when toa is already true
+        bool start_time =0; //start time of measurement usually 0 
+        bool time_interval = 25; // time inverval of measurement ns 
         bitset<8> dec_to_binary_converter(int value){ 
 		if( value <0 ||value > 255) {  cout << "Invalid input number should be 0 to 255" << endl;}
 		bitset <8> binary(value); 
 		return  binary;	
 		} //converts decimal to binary output  8 bit 
+    
+    
+        int GetADC(float value){
+            int ADC_value;
+            float ADC_crude = value/EnergytoADC;
+
+            ADC_value = int(ADC_crude);
+            return ADC_value;
+        }
+        
+
 		bitset<10> ten_to_eight(bitset<8>); //10 bit to 8 bit converter
 		
 	public: 
@@ -24,8 +37,11 @@ class focal_sim_digitizer {
 		void Set_toa_thresh(int value){toa_thresh = value;} //setter for toa threshold 
 		void Set_pedestal(int value){Pedestal = value;}  //setter for pedestal value
 		void Set_EnergytoADC(float value){ EnergytoADC = value;} // set energy to ADC value conversion constant 
-		void main_digitizer(float energy, int channel ); // this outputs whole output with TOT ADC and value for 
-       
+		void Set_Time_Interval(float value){value = time_interval;}// set time interval 
+        void Set_Start_Time(float value) {value = start_time;} //set start time
+        void main_digitizer(float energy, int channel ); // this outputs whole output with TOT ADC and value for
+        void main_digitizer_2(vector<float> energy); // v2 takes vector and spits bunch of ADC(bx) ADC(bx-1) TOT(bx) TOA(bx)  
+        
 
 		
 
--- src/focal_sim_digitizer.cxx
+++ src/focal_sim_digitizer.cxx
@@ -22,3 +22,72 @@ void focal_sim_digitizer::main_digitizer(float energy, int channel){
 
 
 }//end main_digitzer
+
+
+void focal_sim_digitizer::main_digitizer_2(vector<float> energy){
+
+    //start of the mesurement 
+    for(int i=0; i<energy.size();i++){
+        int myadc = GetADC(energy[i]);
+        
+        int toa_value = 0;
+        int tot_value_ = 0;
+        if(myadc<=Pedestal) continue; // no measurement below pedestal
+        bool found_toa = false;
+        bool found_tot_1 = false; // event when tot threshold is crossed
+        bool found_tot_2 = false; //event when tot threhold  crossed back again
+        if(myadc>=toa_thresh){
+            if(!found_toa) toa_value = i*time_interval; //saving the TOA time only if TOA had not been found before
+            found_toa = true;
+            if(!found_tot_1 || !found_tot_2) tot_value_ = i*time_interval;
+            //now two condition of output if belwo TOT threshold then prints ADC(BX) ADC(BX-1) and TOA(BX)
+            if(myadc<tot_thresh){
+               std::cout<< "ADC(BX)      " << "ADC(BX-1)       " << "TOA     "<< std::endl;
+               bitset<8> myfirst_data = dec_to_binary_converter(myadc);
+                bitset <8> toa_bit =  dec_to_binary_converter(toa_value); 
+                
+                
+               if(i==0) std::cout<< myfirst_data<<"    "   << myfirst_data << toa_bit <<"    " <<  std::endl;
+               else{
+                   bitset<8> myfirst_data_ = dec_to_binary_converter(GetADC(energy[i-1])); 
+                   std::cout<< myfirst_data<<"    "  << myfirst_data_  <<"    " <<  toa_bit << std::endl;
+                   } //i !=0  
+            
+            }// myadc< tot_thresh
+            
+            
+            else{
+                found_tot_1 = true;
+                
+                if(found_tot_1){
+                    if(myadc<=tot_thresh) found_tot_2 = true; 
+                    
+                    }// after crossed the tot threshhold
+                int tot_value = tot_value_ - toa_value;
+                std::cout<< "ADC(BX-1)     " << "TOT(BX)     " << "TOA(BX)    "<< std::endl;
+                
+                 bitset<8> tot_bit = dec_to_binary_converter(tot_value);
+                 bitset<8> myfirst_data_ = dec_to_binary_converter(GetADC(energy[i-1]));
+                 bitset <8> toa_bit =  dec_to_binary_converter(toa_value);
+                std::cout<< tot_bit<<"    " << myfirst_data_<<"    " << toa_bit << std::endl;
+            
+            }//myadc>=tot_thresh
+            
+        
+        
+        }//myadc>toa 
+        
+        
+        
+    
+    
+    }//measurement
+    
+    
+    
+    
+    
+    
+
+
+}//end main_digitizer 2
